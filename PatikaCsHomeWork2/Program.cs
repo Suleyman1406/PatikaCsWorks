bafc7e95e2271ae7d7541d3d39d01062443956a7
Your Name <you@example.com>
2022-02-20 21:05:38 +0300

    homework 2 added

---
+++ PatikaCsHomeWork2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            y += 2;
+            y /= 1;
+            x *= 2;
+            Console.WriteLine("x: " + x + " y: " + y);
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfect!");
+
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great!");
+
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine");
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = ++sayi1;
+            Console.WriteLine(sonuc1);
+
+
+        }
+    }
+}
+
