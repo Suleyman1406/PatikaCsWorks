675a5289901ddc9d7a218a44cfd9fbe670319cd9
Your Name <you@example.com>
2022-02-20 21:48:20 +0300

    homework 3 added

---
+++ PatikaCsHomework3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace type_casting
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---- Implict Conversion----");
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d = a + b + c;
+            Console.WriteLine(d);
+
+            long h = d;
+            Console.WriteLine(h);
+
+            float i = h;
+            Console.WriteLine(i);
+
+            string e = "zikriye";
+            char f = 'k';
+            object g = e + f + d;
+            Console.WriteLine(g);
+
+            Console.WriteLine("---- Explicit Conversion----");
+            int x = 4;
+            byte y = (byte)x;
+            Console.WriteLine(y);
+
+            int z = 100;
+            byte t = (byte)z;
+            Console.WriteLine(t);
+
+            float w = 10.3f;
+            byte v = (byte)w;
+            Console.WriteLine(v);
+        }
+
+
+    }
+}
