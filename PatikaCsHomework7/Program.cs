d43a3ea4b6b9ecc6a1a9b94440bff71141baad1b
Your Name <you@example.com>
2022-02-21 02:48:09 +0300

    homework 7 added

---
+++ PatikaCsHomework7/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace loops
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int length = int.Parse(Console.ReadLine() + "");
+                for (int i = 0; i < length; i++)
+                {
+                    if (i % 2 == 1) Console.WriteLine(i);
+                }
+                int evenSum = 0;
+                int oddSum = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    if (i % 2 == 1) oddSum += i;
+                    else evenSum += i;
+
+                }
+                Console.WriteLine("even sum: " + evenSum);
+                Console.WriteLine("odd sum: " + oddSum);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4) break;
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4) continue;
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
