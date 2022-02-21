fb5a95c013525ee3b3f259b4d6b4533c345837c1
Your Name <you@example.com>
2022-02-21 18:56:36 +0300

    homework 11 added

---
+++ PatikaCsHomework11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace methods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(1, 2));
+            Methods m = new Methods();
+            m.Write(Sum(1, 2) + "");
+            m.Write(m.IncreaseSum(2, 3) + "");
+
+        }
+        static int Sum(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+    }
+    class Methods
+    {
+        public void Write(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public int IncreaseSum(int num1, int num2)
+        {
+            return ++num1 + (++num2);
+        }
+    }
+}
