4afbee5494c08a6e752a8e1cd41d65f7ade62246
Your Name <you@example.com>
2022-02-21 03:13:33 +0300

    homework 9 added

---
+++ PatikaCsHomework9/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace arrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] colors = new string[5];
+            string[] animals = { "Cat", "Dog", "Rabbit" };
+            int[] arr;
+            arr = new int[5];
+            colors[0] = "red";
+            arr[3] = 10;
+            Console.WriteLine(animals[0]);
+            Console.WriteLine(arr[3]);
+            Console.WriteLine(colors[0]);
+
+            Console.WriteLine("Please enter the length of array: ");
+            int len = int.Parse(Console.ReadLine() + "");
+            int[] array = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine("Please enter the {0}. item of array: ", i + 1);
+                array[i] = int.Parse(Console.ReadLine() + "");
+            }
+            int total = 0;
+
+            foreach (int item in array)
+            {
+                total += item;
+            }
+            Console.WriteLine("Avarage: " + total / len);
+
+
+        }
+    }
+}
