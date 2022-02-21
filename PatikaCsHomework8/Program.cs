876ba9e1f1abbe3f3a212bb4c6e8cea7445218c6
Your Name <you@example.com>
2022-02-21 03:02:39 +0300

    homework 8 added

---
+++ PatikaCsHomework8/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace loops
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine() + "");
+            int counter = 1;
+            int total = 0;
+            while (num >= counter)
+            {
+                total += counter++;
+            }
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character++);
+            }
+            Console.WriteLine();
+            string[] cars = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
