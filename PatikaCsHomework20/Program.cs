3d057278d3c1def8b539d01b71bcce0cd1b7f171
Your Name <you@example.com>
2022-02-22 21:34:14 +0300

    homework 20 added

---
+++ PatikaCsHomework20/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace classes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Worker w1 = new Worker();
+            w1.name = "ayse";
+            w1.surname = "kara";
+            w1.no = 551651;
+            w1.department = "Purchase department";
+
+            w1.workerInfo();
+        }
+    }
+
+    class Worker
+    {
+        public string name;
+        public string surname;
+        public int no;
+
+        public string department;
+
+        public void workerInfo()
+        {
+            Console.WriteLine("Worker name: " + name);
+            Console.WriteLine("Worker surname: " + surname);
+            Console.WriteLine("Worker no: " + no);
+            Console.WriteLine("Worker department: " + department);
+        }
+
+    }
+}
