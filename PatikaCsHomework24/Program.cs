c60fc4ddb888d8b0df1b4a683d3b484480f89a65
Your Name <you@example.com>
2022-02-23 01:17:14 +0300

    homework 24 added

---
+++ PatikaCsHomework24/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _enum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Sunday);
+            Console.WriteLine((int)Days.Monday);
+        }
+    }
+    enum Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday = 24,
+        Friday,
+        Saturday
+
+    }
+
+    enum WeatherForecast
+    {
+        Cold = 5,
+        Normal = 20,
+        Hot = 25,
+        VeryHot = 30
+
+    }
+}
